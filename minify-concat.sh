c303f6341e357a43c2a1e1d7307e34c95b16650b
Brian Thomas Storti <btstorti@gmail.com>
2015-01-22 01:36:27 -0200

    handle cases when minifier is not available

--- minify-concat.sh
+++ minify-concat.sh
@@ -9,5 +9,10 @@ do
   cat $file".css" >> master.css
 done < manifest
 
-minify master.css
-rm master.css
+if type minify > /dev/null 2>&1; then
+  minify master.css
+  rm master.css
+else
+  echo "\033[1;33m"Minify not found. Try 'npm install -g minifier'"\033[00m";
+  mv master.css master.min.css
+fi
